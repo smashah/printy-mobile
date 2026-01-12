de3cb39ba193dc3d7fd775a090f515a5c4b51a63
M SHAH <shah@idk.uno>
2026-01-12 05:44:17 +0300

    ✨ feat(native): add pnpm symlink resolution support

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,25 +1,47 @@
-const path = require("path");
-const { FileStore } = require('@expo/metro/metro-cache');
+const path = require("node:path");
+const { FileStore } = require("metro-cache");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
 const { getDefaultConfig } = require("expo/metro-config");
+const MetroSymlinksResolver = require("@rnx-kit/metro-resolver-symlinks");
 const { withNativeWind } = require("nativewind/metro");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
+const projectDir = __dirname;
+const workspaceRoot = path.resolve(projectDir, "../..");
 
-const config = getDefaultConfig(projectRoot);
+const symlinksResolver = MetroSymlinksResolver();
 
-config.watchFolders = [workspaceRoot];
+/** @type {import('expo/metro-config').MetroConfig} */
+const expoConfig = getDefaultConfig(projectDir);
 
-config.resolver.nodeModulesPaths = [
-	path.resolve(projectRoot, "node_modules"),
-	path.resolve(workspaceRoot, "node_modules"),
-];
+const config = makeMetroConfig({
+  ...expoConfig,
+  resolver: {
+    ...expoConfig.resolver,
+    resolveRequest: (context, moduleName, platform) => {
+      try {
+        // Symlinks resolver throws when it can't find what we're looking for.
+        const res = symlinksResolver(context, moduleName, platform);
 
-config.resolver.unstable_enablePackageExports = true;
-
-config.cacheStores = [
-  new FileStore({ root: path.join(__dirname, 'node_modules', '.cache', 'metro') }),
-];
+        if (res) {
+          return res;
+        }
+      } catch {
+        // If we have an error, we pass it on to the next resolver in the chain,
+        // which should be one of expos.
+        // https://github.com/expo/expo/blob/9c025ce7c10b23546ca889f3905f4a46d65608a4/packages/%40expo/cli/src/start/server/metro/withMetroResolvers.ts#L47
+        return context.resolveRequest(context, moduleName, platform);
+      }
+    },
+  },
+  watchFolders: [
+    workspaceRoot
+  ],
+  cacheStores: [
+    new FileStore({
+      root: path.join(projectDir, "node_modules", ".cache", "metro"),
+    }),
+  ]
+});
 
 module.exports = withNativeWind(config, {
 	input: "./global.css",
